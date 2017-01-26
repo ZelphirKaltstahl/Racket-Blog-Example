467b4a3dacfaca407b37c5604309a0a5b19ba585
Zelphir Kaltstahl <zelphirkaltstahl@gmail.com>
2017-01-26 23:55:18 +0100

    added comment functionality

--- blog.rkt
+++ blog.rkt
@@ -7,89 +7,122 @@
 (define true #t)
 (define false #f)
 (custodian-limit-memory (current-custodian) MAX-BYTES)
-; racket -l errortrace -t exercise-...
+; racket -l errortrace -t blog.rkt
 ; (provide (all-defined-out))
 
-(struct post (title body))
 (struct blog (posts) #:mutable)
+(struct post (title body comments) #:mutable)
 
 (define BLOG
-  (blog (list (post "First Post!"
-                    "Hey, this is my first post!")
-              (post "Second Post!"
-                    "Haha! Another one!")
-              (post "Third Post!"
-                    "Haha! <b>Another</b> one!"))))
+  (blog
+   (list (post "Second Post"
+               "This is another post"
+               (list))
+         (post "First Post"
+               "This is my first post"
+               (list "First comment!")))))
 
-;; Here the request starts to get handled.
+(define (blog-insert-post! a-blog a-post)
+  (set-blog-posts!
+    a-blog
+    (cons a-post (blog-posts a-blog))))
+
+(define (post-insert-comment! a-post a-comment)
+  (set-post-comments!
+   a-post
+   (cons (post-comments a-post) (list a-comment))))
+
+; start: request -> doesn't return
+; Consumes a request, and produces a page that displays
+; all of the web content.
 (define (start request)
   (render-blog-page request))
 
-;; Renders the blog page and specifies a handler for the form action.
+; render-blog-page: request -> doesn't return
+; Produces an HTML page of the content of the
+; BLOG.
 (define (render-blog-page request)
   (define (response-generator embed/url)
     (response/xexpr
-      `(html
-         (head (title "My Racket Blog"))
-         (body
-           (h1 "My Racket Blog :)")
-           (div ((class "content"))
-             ,(render-posts))
-           (form
-             ((action ,(embed/url insert-post-handler)))
-  
-             (label ((for "title")) "Title:")
-             (input ((name "title") (type "text") (id "title")))
-             (br)
-  
-             (label ((for "body")) "Body:")
-             (input ((name "body") (type "text") (id "body")))
-             (br)
-  
-             (input ((type "submit"))))))))
+     `(html (head (title "My Blog"))
+            (body
+             (h1 "My Blog")
+             ,(render-posts embed/url)
+             (form ((action ,(embed/url insert-post-handler)))
+                   (input ((name "title")))
+                   (input ((name "body")))
+                   (input ((type "submit"))))))))
 
-  ;; render blog page with blog posts plus the new blog post from the request
   (define (insert-post-handler request)
-    (blog-insert-post! (parse-post (request-bindings request)))
+    (blog-insert-post!
+      BLOG
+      (parse-post (request-bindings request)))
     (render-blog-page request))
 
-  ;; ???
   (send/suspend/dispatch response-generator))
 
-(define (render-posts)
-  ;; This function uses the ,@ notation.
-  ;; The backtick allows for S-Expressions to be evaluated inside the quoted expression.
-  ;; , allows evaluating of an S-Expression inside the backtick quoted expression
-  ;; , does however unquote the complete given S-Expression
-  ;; We do not want to unquote the whole sub expressions though.
-  ;; We only want to splice them into our code.
-  ;; So we have to use ,@ instead.
-  `(div ((class "posts-container"))
-     ,@(map render-post (blog-posts BLOG))))
-
-;; renders a post as HTML
-(define (render-post a-post)
-  `(div ((class "post"))
-     (h1 ,(post-title a-post))
-     (p ,(post-body a-post))))
-
-;; checks if bindings contain the symbols of a post
-(define (can-parse-post? bindings)
-  (and
-    (exists-binding? 'title bindings)
-    (exists-binding? 'body bindings)))
-
-;; creates a post from bindings
+; parse-post: bindings -> post
+; Extracts a post out of the bindings.
 (define (parse-post bindings)
-  (post
-    (extract-binding/single 'title bindings)
-    (extract-binding/single 'body bindings)))
+  (post (extract-binding/single 'title bindings)
+        (extract-binding/single 'body bindings)
+        (list)))
 
-;; ATTENTION: THIS FUNCTION HAS SIDE EFFECTS!!!
-;; ALSO FUNCTIONS USING THIS FUNCTION HAVE SIDE EFFECTS!!!
-(define (blog-insert-post! a-post)
-  (set-blog-posts!
-    BLOG
-    (cons a-post (blog-posts BLOG))))
+; render-post-detail-page: post request -> doesn't return
+; Consumes a post and request, and produces a detail page
+; of the post. The user will be able to insert new comments.
+(define (render-post-detail-page a-post request)
+  (define (response-generator embed/url)
+    (response/xexpr
+     `(html (head (title "Post Details"))
+            (body
+             (h1 "Post Details")
+             (h2 ,(post-title a-post))
+             (p ,(post-body a-post))
+             ,(render-as-itemized-list
+               (post-comments a-post))
+             (form ((action
+                     ,(embed/url insert-comment-handler)))
+                   (input ((name "comment")))
+                   (input ((type "submit"))))))))
+
+  (define (parse-comment bindings)
+    (extract-binding/single 'comment bindings))
+
+  (define (insert-comment-handler a-request)
+    (post-insert-comment!
+     a-post (parse-comment (request-bindings a-request)))
+    (render-post-detail-page a-post a-request))
+  (send/suspend/dispatch response-generator))
 
 
+; render-post: post (handler -> string) -> xexpr
+; Consumes a post, produces an xexpr fragment of the post.
+; The fragment contains a link to show a detailed view of the post.
+(define (render-post a-post embed/url)
+  (define (view-post-handler request)
+    (render-post-detail-page a-post request))
+  `(div ((class "post"))
+        (a ((href ,(embed/url view-post-handler)))
+          (h1 ,(post-title a-post)))
+        (p ,(post-body a-post))
+        (div ,(number->string (length (post-comments a-post)))
+             " comment(s)")))
+
+; render-posts: (handler -> string) -> xexpr
+; Consumes a embed/url, and produces an xexpr fragment
+; of all its posts.
+(define (render-posts embed/url)
+  ;; We only define this function, so that we can use it
+  ;; in the application of map on the list of blog posts.
+  (define (render-post-with-embed-url a-post)
+    (render-post a-post embed/url))
+
+  `(div ((class "posts"))
+    ,@(map render-post-with-embed-url (blog-posts BLOG))))
+
+(define (render-as-itemized-list fragments)
+  `(ul ,@(map render-as-item fragments)))
+
+(define (render-as-item a-fragment)
+  `(li ,a-fragment))
